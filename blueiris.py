1446164a9d45dcf08a55be76449dbf9c3091c318
Nicholas Westerhausen <nicholas@westerhausen.me>
2019-01-04 19:36:14 -0500

    Added constants Put in constants for the PTZ options Put in constants for SIGNAL_RED/GREEN/YELLOW

--- blueiris.py
+++ blueiris.py
@@ -11,12 +11,74 @@ import requests
 
 _LOGGER = logging.getLogger(__name__)
 
-SIGNALS = ['red', 'green', 'yellow']
+SIGNAL_RED = 'red'
+SIGNAL_YELLOW = 'yellow'
+SIGNAL_GREEN = 'green'
 
+SIGNALS = [SIGNAL_RED, SIGNAL_GREEN, SIGNAL_YELLOW]
+
+PTZ_PAN_LEFT = 0
+PTZ_PAN_RIGHT = 1
+PTZ_TILT_UP = 2
+PTZ_TILT_DOWN = 3
+PTZ_CENTER = PTZ_HOME = 4
+PTZ_ZOOM_IN = 5
+PTZ_ZOOM_OUT = 6
+PTZ_POWER_50 = 8
+PTZ_POWER_60 = 9
+PTZ_POWER_OUTDOOR = 10
+PTZ_BRIGHTNESS_0 = 11
+PTZ_BRIGHTNESS_1 = 12
+PTZ_BRIGHTNESS_2 = 13
+PTZ_BRIGHTNESS_3 = 14
+PTZ_BRIGHTNESS_4 = 15
+PTZ_BRIGHTNESS_5 = 16
+PTZ_BRIGHTNESS_6 = 17
+PTZ_BRIGHTNESS_7 = 18
+PTZ_BRIGHTNESS_8 = 19
+PTZ_BRIGHTNESS_9 = 20
+PTZ_BRIGHTNESS_10 = 21
+PTZ_BRIGHTNESS_11 = 22
+PTZ_BRIGHTNESS_12 = 23
+PTZ_BRIGHTNESS_13 = 24
+PTZ_BRIGHTNESS_14 = 25
+PTZ_BRIGHTNESS_15 = 26
+PTZ_CONTRAST_0 = 27
+PTZ_CONTRAST_1 = 28
+PTZ_CONTRAST_2 = 29
+PTZ_CONTRAST_3 = 30
+PTZ_CONTRAST_4 = 31
+PTZ_CONTRAST_5 = 32
+PTZ_CONTRAST_6 = 33
+PTZ_IR_ON = 34
+PTZ_IR_OFF = 35
+PTZ_PRESET_1 = 101
+PTZ_PRESET_2 = 102
+PTZ_PRESET_3 = 103
+PTZ_PRESET_4 = 104
+PTZ_PRESET_5 = 105
+PTZ_PRESET_6 = 106
+PTZ_PRESET_7 = 107
+PTZ_PRESET_8 = 108
+PTZ_PRESET_9 = 109
+PTZ_PRESET_10 = 110
+PTZ_PRESET_11 = 111
+PTZ_PRESET_12 = 112
+PTZ_PRESET_13 = 113
+PTZ_PRESET_14 = 114
+PTZ_PRESET_15 = 115
+PTZ_PRESET_16 = 116
+PTZ_PRESET_17 = 117
+PTZ_PRESET_18 = 118
+PTZ_PRESET_19 = 119
+PTZ_PRESET_20 = 120
 
 class BlueIris:
 
-    def __init__(self, protocol, host, user, password, debug=False):
+    def __init__(self, user, password, protocol, host, port="", debug=False):
+        if port != "":
+            host = "{}:{}".format(host, port)
+
         self.debug = debug
         self.url = "{}://{}/json".format(protocol, host)
         self.session = requests.session()
@@ -35,7 +97,7 @@ class BlueIris:
         self.login()
 
     @property
-    def system_name(self):
+    def name(self):
         """Return the system name"""
         return self._system_name
 
@@ -116,7 +178,7 @@ class BlueIris:
         r = self.session.post(self.url,
                               data=json.dumps({"cmd": "login", "session": self.sessionid, "response": self.response}))
         if r.status_code != 200 or r.json()["result"] != "success":
-            print("Unsuccessful response. {}:{}".format(r.status_code, r.text))
+            print("Unsuccessful response {} :{}".format(r.status_code, r.text))
         else:
             self._system_name = r.json()["data"]["system name"]
             self._profiles_list = r.json()["data"]["profiles"]
